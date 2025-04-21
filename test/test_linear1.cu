e936bb4540c224abcc1d9d3fec295e55bffe9596
Pavan Thodima <pthodima@gmail.com>
2025-04-21 13:35:24 -0500

    Fix gradient retrieval for backwardGPU Linear

--- test/test_linear1.cu
+++ test/test_linear1.cu
@@ -154,8 +154,8 @@ void test_backward_gpu() {
     float expected_dW[] = {0.5*1, 1*(-1.0), 0.5*2, -1.0*2};  // xᵗ * grad_out
     float expected_db[] = {0.5f, -1.0f};
 
-    for(int i = 0; i < 4; i++) std::cout << "W[" << i << "] : " << layer.host_grad_weights[i] << std::endl;
-    for(int i = 0; i < 2; i++) std::cout << "b[" << i << "] : " << layer.host_grad_biases[i] << std::endl;
+    for(int i = 0; i < 4; i++) std::cout << "W[" << i << "] : " << layer.device_grad_weights[i] << std::endl;
+    for(int i = 0; i < 2; i++) std::cout << "b[" << i << "] : " << layer.device_grad_biases[i] << std::endl;
 
     for (int i = 0; i < 4; i++) assert(std::abs(grad_w[i] - expected_dW[i]) < 1e-4);
     for (int i = 0; i < 2; i++) assert(std::abs(grad_b[i] - expected_db[i]) < 1e-4);
