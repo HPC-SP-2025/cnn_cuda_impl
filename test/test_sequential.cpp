3172cf125b026408a5d448fc762777f6581aaadc
Arshad-Kazi <arshad1kazi@gmail.com>
2025-04-21 14:31:02 -0500

    Cmake file added

--- test/test_sequential.cpp
+++ test/test_sequential.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "../include/cnn_library/nn/sequential.h"
+#include "../include/cnn_library/layers/relu.h"
+// #include "../include/cnn_library/layers/softmax.h"
 
 using namespace std;
 
@@ -9,6 +11,9 @@ int main(){
     // Create a Sequential model
     Sequential* model = new Sequential(784, 10); // Example input size of 784 (28x28 image flattened) and output size of 10 (number of classes)
     cout << "Model created with input size: " << model->getInputSize() << " and output size: " << model->getOutputSize() << endl;
+    cout << "Adding layers..." << endl;
+    // Add layers to the model
+    
 
     return 0;
 }
